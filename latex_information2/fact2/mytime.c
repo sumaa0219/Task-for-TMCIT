a7e64ac71521af19e9f7d61dd8f8708a4abc3ff9
sumaa0219 <82751329+sumaa0219@users.noreply.github.com>
2022-05-24 12:43:07 +0900

    succes

--- latex_information2/fact2/mytime.c
+++ latex_information2/fact2/mytime.c
@@ -13,3 +13,34 @@ void makeTimeFromSeconds(int *ansp, int seconds) {
 void makeTime(int *ansp, int h, int m, int s) {
     makeTimeFromSeconds(ansp, getSeconds(h, m, s));
 }
+
+int timeCmp(int *t1p, int *t2p) {
+    int time1, time2;
+    time1 = getSecondsFromTime(t1p);
+    time2 = getSecondsFromTime(t2p);
+    return time1 > time2 ? 1 : time1 < time2 ? -1 : 0;
+}
+
+void addTime(int *ansp, int *t1p, int *t2p) {
+    makeTimeFromSeconds(ansp,
+                        getSecondsFromTime(t1p) + getSecondsFromTime(t2p));
+}
+
+void mulTime(int *ansp, int *tp, int n) {
+    makeTimeFromSeconds(ansp, getSecondsFromTime(tp) * n);
+}
+
+void divTime(int *ansp, int *tp, int n) {
+    makeTimeFromSeconds(ansp, getSecondsFromTime(tp) / n);
+}
+
+int divTimeByTime(int *t1p, int *t2p) {
+    return (getSecondsFromTime(t1p) / getSecondsFromTime(t2p));
+}
+
+void subTime(int *ansp, int *t1p, int *t2p) {
+    makeTimeFromSeconds(
+        ansp, (((getSecondsFromTime(t1p) - getSecondsFromTime(t2p)) / 2) +
+               getSecondsFromTime(t1p)) *
+                  timeCmp(t1p, t2p));
+}
--- latex_information2/fact2/testmytime.c
+++ latex_information2/fact2/testmytime.c
@@ -35,10 +35,89 @@ void testMakeTimeFromSeconds() {
     assertEqualsInt(jikoku[SS], 59);
 }
 
+void testTimeCmp() {
+    testStart("TimeCmp");
+    int jikoku1[N] = {2, 34, 56}, jikoku2[N] = {11, 59, 59};
+    assertEqualsInt(timeCmp(jikoku1, jikoku2), -1);
+    assertEqualsInt(timeCmp(jikoku2, jikoku1), 1);
+    assertEqualsInt(timeCmp(jikoku1, jikoku1), 0);
+}
+
+void testaddTime() {
+    testStart("addTime");
+    int jikoku1[N] = {2, 34, 56}, jikoku2[N] = {11, 59, 59},
+        jikoku3[N] = {0, 0, 0};
+    addTime(jikoku3, jikoku1, jikoku2);
+    assertEqualsInt(jikoku3[HH], 14);
+    assertEqualsInt(jikoku3[MM], 34);
+    assertEqualsInt(jikoku3[SS], 55);
+    addTime(jikoku1, jikoku2, jikoku3);
+    assertEqualsInt(jikoku1[HH], 26);
+    assertEqualsInt(jikoku1[MM], 34);
+    assertEqualsInt(jikoku1[SS], 54);
+}
+
+void testmulTime() {
+    testStart("mulTime");
+    int jikan1[N] = {2, 34, 56}, jikan2[N] = {0, 0, 0};
+    mulTime(jikan2, jikan1, 2);
+    assertEqualsInt(jikan2[HH], 5);
+    assertEqualsInt(jikan2[MM], 9);
+    assertEqualsInt(jikan2[SS], 52);
+    mulTime(jikan2, jikan1, 3);
+    assertEqualsInt(jikan2[HH], 7);
+    assertEqualsInt(jikan2[MM], 44);
+    assertEqualsInt(jikan2[SS], 48);
+}
+
+void testdivTime() {
+    testStart("divTime");
+    int jikan1[N] = {2, 34, 56}, jikan2[N] = {0, 0, 0};
+    divTime(jikan2, jikan1, 2);
+    assertEqualsInt(jikan2[HH], 1);
+    assertEqualsInt(jikan2[MM], 17);
+    assertEqualsInt(jikan2[SS], 28);
+    divTime(jikan2, jikan1, 3);
+    assertEqualsInt(jikan2[HH], 0);
+    assertEqualsInt(jikan2[MM], 51);
+    assertEqualsInt(jikan2[SS], 38);
+}
+
+void testDivTimeByTime() {
+    testStart("divTimeByTime");
+    int jikan1[N] = {2, 34, 56}, jikan2[N] = {11, 59, 59};
+    assertEqualsInt(divTimeByTime(jikan2, jikan1), 4);
+    assertEqualsInt(divTimeByTime(jikan1, jikan2), 0);
+}
+
+void testSubTime() {
+    testStart("subTime");
+    int jikan1[N] = {2, 34, 56}, jikan2[N] = {11, 59, 59},
+        jikan3[N] = {0, 0, 0};
+    subTime(jikan3, jikan2, jikan1);
+    assertEqualsInt(jikan3[HH], 16);
+    assertEqualsInt(jikan3[MM], 42);
+    assertEqualsInt(jikan3[SS], 30);
+    subTime(jikan3, jikan1, jikan2);
+    assertEqualsInt(jikan3[HH], 2);
+    assertEqualsInt(jikan3[MM], 7);
+    assertEqualsInt(jikan3[SS], 35);
+    subTime(jikan3, jikan1, jikan1);
+    assertEqualsInt(jikan3[HH], 0);
+    assertEqualsInt(jikan3[MM], 0);
+    assertEqualsInt(jikan3[SS], 0);
+}
+
 int main() {
     testGetSeconds();
     testGetSecondsFromTime();
     testMakeTimeFromSeconds();
+    testTimeCmp();
+    testaddTime();
+    testmulTime();
+    testdivTime();
+    testDivTimeByTime();
+    testSubTime();
     testErrorCheck();
     return 0;
 }
